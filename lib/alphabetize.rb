3b828342bc98e43bcedc55cdcb859bf6984fb580
Sherzod Karimov <karimovj@gmail.com>
2018-07-25 21:58:51 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
 def alphabetize(arr)
-  # code here
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alph_arr = ESPERANTO_ALPHABET.split("")
+  
+  arr.sort_by do |word|
+    word.each do |letter|
+      
 end
