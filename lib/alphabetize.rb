188adb06074ea742e2fffd56d951f8e5c4f0b054
Sherzod Karimov <karimovj@gmail.com>
2018-07-25 22:09:46 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
+require 'pry'
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alph_arr = esperanto_alphabet.split("")
+  
+  arr.sort_by do |words|
+    words.split('').collect do |letter|
+      alph_arr.index(letter)
+    end
+  end
 end
