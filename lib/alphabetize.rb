ff62e88fabd8c948cc43d201b24bdbe193456558
Sherzod Karimov <karimovj@gmail.com>
2018-07-25 22:02:10 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,9 +2,9 @@ def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   alph_arr = esperanto_alphabet.split("")
   
-  arr.sort_by do |word|
-    word.each do |letter|
-      alph_arr.index(letter)
+  arr.sort_by do |words|
+    word.each do |word|
+      word_idx = word.split('').alph_arr.index(letter)
       word_idx
     end
   end
