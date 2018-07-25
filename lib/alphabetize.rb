35c2d8ac2e2f589c1257cd8a6455d28b155def03
Sherzod Karimov <karimovj@gmail.com>
2018-07-25 22:03:40 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,8 +4,11 @@ def alphabetize(arr)
   
   arr.sort_by do |words|
     word.each do |word|
-      word_idx = word.split('').alph_arr.index(letter)
-      word_idx
+      word_letter = word.split('')
+        word_letter.each do |letter|
+        word_idx = alph_arr.index(letter)
+        word_idx
+      end
     end
   end
 end
